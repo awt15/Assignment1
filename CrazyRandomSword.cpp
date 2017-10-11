f2be5805c9482728890b1950f0380f9cdd85b010
Anthony Wain Tieu <atieu@linprog2.cs.fsu.edu>
2017-10-11 19:15:25 -0400

    Updated CrazyRandomSword
    Updated the CrazyRandomSword class to work.

--- CrazyRandomSword.cpp
+++ CrazyRandomSword.cpp
@@ -7,7 +7,6 @@ Anthony Tieu
 
 double CrazyRandomSword::hit(double armor)
 {
-	hitPoints = rand()% 91 + 10;
 	int halfarmor = armor/2 + 1;
 	int reduce = rand()% halfarmor;
 
--- CrazyRandomSword.h
+++ CrazyRandomSword.h
@@ -5,6 +5,7 @@ Anthony Tieu
 
 #include <string>
 #include <cstdlib>
+#include <time.h>
 #include "Weapon.h"
 
 #ifndef CRAZYRANDOMSWORD_H
@@ -13,7 +14,11 @@ Anthony Tieu
 class CrazyRandomSword: public Weapon
 {
 	public:
-		CrazyRandomSword() : Weapon("Crazy Random Sword", rand()%91+10) {};
+		CrazyRandomSword() : Weapon("Crazy Random Sword", 0)
+		{
+			srand(time(NULL));
+			hitPoints = (rand()%91+10);
+		};
 		virtual ~CrazyRandomSword() {};
 		virtual double hit(double armor);
 };
